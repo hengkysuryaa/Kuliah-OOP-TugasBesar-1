ec925449843daa6bb1cb0cdc63ac3d195ea1973c
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-03-22 15:42:12 +0700

    fix apptest

--- src/test/java/oop/tubes1/AppTest.java
+++ src/test/java/oop/tubes1/AppTest.java
@@ -7,9 +7,11 @@ import java.lang.reflect.Field;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
-import oop.tubes1.exception.expression.ExpressionException;
 import oop.tubes1.exception.expression.DivisionByZeroExpressionException;
+import oop.tubes1.exception.expression.ExpressionException;
 import oop.tubes1.exception.expression.NegativeRootExpressionException;
 import oop.tubes1.exception.input.CommaInputException;
 import oop.tubes1.exception.input.InputException;
@@ -28,19 +30,25 @@ public class AppTest {
         app = new CalculatorApp();
     }
 
+    @BeforeEach
+    public void reset() {
+        app.getMemoryDisplay().clear();
+        app.getCalculatorDisplay().clear();
+    }
+
     @Test
     public void _1() {
-        error("%", OperatorInputException.class, "%");
+        error("%=", OperatorInputException.class, "%");
     }
 
     @Test
     public void _2() {
-        test("5%", 0.05);
+        test("5%=", 0.05);
     }
 
     @Test
     public void _3() {
-        error(".5", CommaInputException.class, ".5");
+        test(".5=", 0.5);
     }
 
     @Test
@@ -50,17 +58,17 @@ public class AppTest {
 
     @Test
     public void _5() {
-        error("√-5", NegativeRootExpressionException.class, -5d);
+        error("√-5=", NegativeRootExpressionException.class, -5d);
     }
 
     @Test
     public void _6() {
         error("2+5-7x8/0=", DivisionByZeroExpressionException.class, 56d);
     }
-    
+
     @Test
     public void _7() {
-        error("√0/0", DivisionByZeroExpressionException.class, 0d);
+        error("√0/0=", DivisionByZeroExpressionException.class, 0d);
     }
 
     @Test
@@ -77,7 +85,7 @@ public class AppTest {
     public void _10() {
         test("10-5=", 5.0);
     }
-    
+
     @Test
     public void _11() {
         test("5x5/10=", 2.5);
@@ -85,7 +93,7 @@ public class AppTest {
 
     @Test
     public void _12() {
-        test("√25", 5);
+        test("√25=", 5);
     }
 
     @Test
@@ -95,63 +103,73 @@ public class AppTest {
 
     @Test
     public void _14() {
-        error("--12", OperatorInputException.class, "--");
+        error("--12=", OperatorInputException.class, "--");
     }
 
     @Test
     public void _15() {
-        error("5.1.2+2", CommaInputException.class, "5.1.2");
+        error("5.1.2+2=", CommaInputException.class, "5.1.2");
     }
 
     @Test
     public void _16() {
-        test("7x5xAns", 0);
+        // test("7x5xAns=", 0);
+        reset();
+        input("7x5x");
+        press("Ans");
+        press('=');
+        checkRes(0);
     }
 
     @Test
     public void _20() {
-        test("√√5", 1.4953487812212205);
+        test("√√5=", 1.4953487812212205);
     }
 
     @Test
     public void _21() {
-        test("5x√2", 7.0710678118654755);
+        test("5x√2=", 7.0710678118654755);
     }
-   
+
     @Test
     public void _22() {
-        error("5√2", OperatorInputException.class, "5√");
+        error("5√2=", OperatorInputException.class, "5√");
     }
-   
+
     private void test(String exp, double res) {
-        calculateInput(exp);
+        reset();
+        input(exp);
+        checkRes(res);
+    }
+
+    private void checkRes(double res) {
         assertEquals(Double.parseDouble(app.getCalculatorDisplay().getText()), res, PRECISION);
     }
 
     private void error(String exp, Class<?> errorClass, Object message) {
+        boolean errorFound = false;
         try {
-            calculateInput(exp);
-            new MathEvaluator(exp).getExpression().solve();
+            input(exp);
+            new MathEvaluator(exp.endsWith("=") ? exp.substring(0, exp.length() - 1) : exp).getExpression().solve();
         } catch (InputException e) {
             assertEquals(e.getExpression(), message);
             assertTrue(errorClass.isInstance(e));
+            errorFound = true;
         } catch (ExpressionException e) {
             assertEquals(e.getNumber(), (Double) message, PRECISION);
             assertTrue(errorClass.isInstance(e));
+            errorFound = true;
         } catch (Exception e) {
             assertEquals(e.getMessage(), message);
             assertTrue(errorClass.isInstance(e));
+            errorFound = true;
         }
+        assertTrue(errorFound);
     }
 
-    private void calculateInput(String exp) {
-        app.getMemoryDisplay().clear();
-        app.getCalculatorDisplay().clear();
+    private void input(String exp) {
         for (char c : exp.toCharArray()) {
             press(c);
-        }
-        if (exp.charAt(exp.length() - 1) != '=') {
-            press('=');
         }
     }
 
